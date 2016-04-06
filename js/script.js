8d109a7fb9d5f896d1a00100bb8320ae00fe7aa1
jllrk1 <jlark002@my.hennepintech.edu>
2016-04-06 17:30:20 -0500

    changes made could not figure out howtoget Getweather app function to load

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-$.ready(function() {
-    
-   console.log("hello"); 
-    
-    
-});
+$(function () {
+     		 
+    console.log("hello"); 		 +
+     		 	//Function to get json data
+     		 	function getWeatherData(zipcode) {
+ 		 		var apikey = $('#apikey').val();
+ 		console.log("apikey=" + apikey);
+ 		console.log("zipcode=" + zipcode);
+ 		var weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&appid=' + apikey;
+ 		$.getJSON(weatherApi)
+ 			.done(function (data) {
+ 			addNewItem(data);
+ 			}).fail(function () {
+ 				$('#form').append('<p> Can not load data </p>');
+ 			});
+ 	}
+ 	$('form').on('submit', function (e) {
+ 		e.preventDefault();
+ 		$('#weather').remove();
+ 		getWeatherData($('#zip').val());
+ 	});
+ 	
+ 	function addNewItem(data){
+ 		data += '<h3>The weather info for' + ' ' + data.name + '</h3>';
+ 		data += '<p>Current Temperature: ' + convertTemp(data.main.temp) + '</p>';
+ 		data += '<p>' + data.weather[0].description + '</p>';
+ 		$('#form').append('<div id = weather>' + data + '</div>');
+ 	}
+ 	function convertTemp (temp){
+ 		return Math.round((temp * 1.8) - 459.67);
+ 	}
+ 	
+ });
